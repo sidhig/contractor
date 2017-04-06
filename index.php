ab5ac9e1f052a2a4dff49055593f57d1e10d1476
sidhig <sidhiagra@gmail.com>
2017-04-06 12:54:25 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,304 +1,140 @@
-<?
-session_start();
-//echo $_SESSION["db_name"];
+<?php
+include_once 'common.php';
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
 
-//print_r($_POST);
 
-if(empty($_POST)){
-  $_SESSION["db_name"]='db_admin';
- 
-}
-else{
-  $_SESSION["db_name"] = explode('/',$_POST['cmpnyname'])[0];
-  $_SESSION["account_id"] = explode('/',$_POST['cmpnyname'])[1];
-}
- include_once('connect.php');
-if(!empty($_POST))
-{
-  //echo "SELECT  * FROM tbl_user where username='".$_POST['username']."' AND password ='".md5($_POST['password'])."'";
-    $sql = "SELECT  * FROM tbl_user where username='".$_POST['username']."' AND password ='".md5($_POST['password'])."'";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) 
-    {
-     // $row = $result->fetch_object();
-      $row = $result->fetch_object();
-      $_SESSION["username"]=$row->username;
-      header("location: home.php");
-    }
-  else
-  {
-    $err="1";
-  } 
-  //unset($_POST);
- $conn->close();    
-//$conn = null;   
-}
-else
-{
-  $err ="";
-}
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="Generator" content="Design-Lib.Com - Basic Web Page Layouts Creator 1.0">
+<title><?php echo $lang['PAGE_TITLE']; ?></title>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all">
 
-?> 
+</head><body>
 
-<!DOCTYPE html>
-<html>
 
-<head>
 
-  <meta charset="UTF-8">
-	
-  <title>Contractor</title>
-   <script src="js/prefixfree.min.js"></script>
-   <script src="js/jquery.min.1.12.0.js"></script>
-   <link rel="shortcut icon" type="image/png" href="image/favicon.png"/>
-<script>
-  function validateForm() {
-    if ($("#comp_name").val().trim() == "") 
-    {
-      $("#comp_name").css('border','1px solid red');
-      $("#comp_name").focus();
-      /*if ($("#password").val().trim() == "")
-        $("#password").css('border','2px solid red');*/
-    }     
-   else if ($("#username").val().trim() == "") 
-    {
-        $("#username").css('border','2px solid red');
-        $("#username").focus();
-    }
-    else if ($("#password").val().trim() == "") 
-    {
-        $("#password").css('border','2px solid red');
-        $("#password").focus();
-    }
-    else
-    {    
-      $("#spinner").show();
-      $("#myform").submit();
-    }
-  }
+<div id="OuterFrmDl"><!-- OuterFrmDl BEGIN -->
 
-  $(document).ready(function(){
-    $("#comp_name").focus();
-      $("#comp_name").on('input',function(e){
-         $("#comp_name").css('border','1px solid #CCC');
-          $("#error").hide();
-        });
-    //$("#username").focus();
-      $("#username").on('input',function(e){
-         $("#username").css('border','1px solid #CCC');
-          $("#error").hide();
-        });
-      $("#password").on('input',function(e){
-         $("#password").css('border','1px solid #CCC');
-          $("#error").hide();
-      });
-  });
-$(function() {
+<div id="HeadSectionDl">
 
-    //$("#button").click(function() {
-    $("#comp_name").on('input', function () {
-        var val = $('#comp_name').val()
-        var xyz = $('#CompanyName option').filter(function() {
-            return this.value == val;
-        }).data('xyz');
-        var msg = xyz ? xyz : 'No Match';
-        $('#cmpnyname').val(msg);
+<!-- HeadSectionDl BEGIN -->
+<div id="languages">
+<a href="index.php?lang=en"><img src="images/en.png" /></a>
+<a href="index.php?lang=de"><img src="images/de.png" /></a>
+<a href="index.php?lang=es"><img src="images/es.png" /></a>
+</div>
 
-    })
+  <div id="WSTitleDL"><?php echo $lang['HEADER_TITLE']; ?></div>
+  <div id="WSubTitleDL"><?php echo $lang['SLOGAN']; ?></div>
+</div>
+<!-- HeadSectionDl END -->
 
-	$('#username,#password').on('keypress', function (e) {
-	  if (e.which == 13) {
-	   validateForm();
-		return false;    //<---- Add this line onclick="validateForm()"
-	  }
-	});
+<div id="TopHzNavSectionDl">
+<!-- HeadSectionDl BEGIN -->
+  <div class="HzNavCntDL">
+    <ul>
+      <li><a href="#"><?php echo $lang['MENU_HOME']; ?></a></li>
+      <li><a href="#"><?php echo $lang['MENU_ABOUT_US']; ?></a></li>
+	  <li><a href="#"><?php echo $lang['MENU_OUR_PRODUCTS']; ?></a></li>
+      <li><a href="#"><?php echo $lang['MENU_CONTACT_US']; ?></a></li>
+      <li><a href="#"><?php echo $lang['MENU_ADVERTISE']; ?></a></li>	
+      <li><a href="#"><?php echo $lang['MENU_SITE_MAP']; ?></a></li>
+    </ul>
+  </div>
+</div>
+<!-- HeadSectionDl END -->
+<div id="CntSectionDlDiv"><!-- CntSectionDlDiv BEGIN -->
 
-})
+  <table id="CntSectionDlTbl" cellpadding="0" cellspacing="0"><!-- CntSectionDlTbl -->
+    <tbody><tr>
+      <td class="LeftSideBar" valign="top">
+	<div class="VtNavCntDL">
+    <ul>
+      <li><a href="#" title="Link 1 Title">Sample Side Link 1</a></li>
+      <li><a href="#" title="Link 2 Title">Sample Side Link 2</a></li>
+      <li><a href="#" title="Link 3 Title">Sample Side Link 3</a></li>
+      <li><a href="#" title="Link 4 Title">Sample Side Link 4</a></li>	
+      <li><a href="#" title="Link 5 Title">Sample Side Link 5</a></li>  
+    </ul>
+  </div>	  
+	  <div class="ContentBoxDl">
+        <h3><?php echo $lang['HEADING']; ?> 3 </h3>
+	    <p>
+Lorem ipsum dolor sit amet eget nunc. Donec ultricies fermentum justo.
+Sed ultrices. Praesent scelerisque rutrum nibh. Quisque quam. Aenean
+posuere bibendum lectus. Donec <a href="#" title="The Link Title">congue</a>
+nisi non nulla. In hac habitasse platea dictumst. Vivamus pretium. Ut
+consequat fringilla tellus. Vivamus semper sodales libero. Ut in ligula
+in massa elementum condimentum. Proin pulvinar neque et pede. Donec at
+quam ut lectus molestie lacinia. Sed sapien. Mauris venenatis leo quis
+mauris. </p>
+	    </div></td>	  
+      <td class="ContentsArea" valign="top">
+      
+      <div class="ContentBoxDl">
+        <h1><?php echo $lang['HEADING']; ?> 1 </h1>
+        <p> Lorem ipsum dolor sit amet, <a href="#" title="The Link Title">consectetuer</a>
+adipiscing elit. Pellentesque at libero. In turpis ante, pretium id,
+consectetuer nec, porta a, nibh. Quisque malesuada massa elementum
+tortor. Sed nulla dui, faucibus ac, euismod ac, elementum non, lorem.
+Sed nec nibh. Vivamus malesuada neque vel velit. Vivamus tempus dolor
+non mi mollis dapibus. Pellentesque habitant morbi tristique senectus
+et netus et malesuada fames ac turpis egestas. Praesent neque ante,
+rutrum eget, sodales vitae, facilisis gravida, ligula. Curabitur sed
+felis. </p>
+        <p>&nbsp;</p>
+        <h2><?php echo $lang['HEADING']; ?> 2 </h2>
+        <p>Nulla suscipit magna suscipit eros. Ut aliquam viverra   quam. Etiam vel ipsum et massa tincidunt tempus. <a href="#" title="The Link Title">Nullam</a>
+feugiat velit id neque. Nunc nec felis. Sed vitae nibh vitae nisi
+ultricies pretium. Vestibulum tincidunt. Aenean dapibus mollis libero.
+Duis lacus urna, varius vel, molestie varius, imperdiet at, orci.
+Maecenas eu nisl. Vestibulum ante ipsum primis in faucibus orci luctus
+et ultrices posuere cubilia Curae; Nam laoreet justo. Cras et ante nec
+tellus vestibulum fringilla. Pellentesque sem. In fermentum, elit vel
+condimentum dignissim, magna nulla aliquam nisi, eu hendrerit eros
+turpis rhoncus sapien. Mauris laoreet lacinia elit. Quisque tristique
+turpis a nibh. Phasellus consequat nunc et felis. Suspendisse ut lorem
+et velit volutpat consectetuer. </p>
+<p>&nbsp;</p>
+        <p>Duis molestie sem eget nunc. Donec ultricies
+fermentum justo. Sed ultrices. Praesent scelerisque rutrum nibh.
+Quisque quam. Aenean posuere bibendum lectus. Donec congue nisi non
+nulla. In hac habitasse platea dictumst. Vivamus pretium. Ut consequat
+fringilla tellus. Vivamus semper sodales libero. Ut in ligula in massa
+elementum condimentum. Proin pulvinar neque et pede. Donec at quam ut
+lectus molestie lacinia. Sed sapien. Mauris venenatis leo quis mauris. </p>
+      </div>
+</td>
+	  
+        <td class="RightSideBar" valign="top"><div class="VtNavCntDL">
+            <ul>
+      <li><a href="#" title="Link 1 Title">Sample Side Link 1</a></li>
+      <li><a href="#" title="Link 2 Title">Sample Side Link 2</a></li>
+      <li><a href="#" title="Link 3 Title">Sample Side Link 3</a></li>
+      <li><a href="#" title="Link 4 Title">Sample Side Link 4</a></li>	
+      <li><a href="#" title="Link 5 Title">Sample Side Link 5</a></li>
+            </ul>
+          </div>
+          <div class="ContentBoxDl">
+            <h3><?php echo $lang['HEADING']; ?> 3 </h3>
+            <p>
+Lorem ipsum dolor sit amet eget nunc. Donec ultricies fermentum justo.
+Sed ultrices. Praesent scelerisque rutrum nibh. Quisque quam. Aenean
+posuere bibendum lectus. Donec <a href="#" title="The Link Title">congue</a>
+nisi non nulla. In hac habitasse platea dictumst. Vivamus pretium. Ut
+consequat fringilla tellus. Vivamus semper sodales libero. Ut in ligula
+in massa elementum condimentum. Proin pulvinar neque et pede. Donec at
+quam ut lectus molestie lacinia. Sed sapien. Mauris venenatis leo quis
+mauris. </p>
+          </div></td>
+    </tr>
+  </tbody></table><!-- CntSectionDlTbl -->
 
-</script>
-    <style>
-@import url(https://fonts.googleapis.com/css?family=Exo:100,200,400);
-@import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:700,400,300);
+</div><!-- CntSectionDlDiv END -->
 
-body{
-  margin: 0;
-  padding: 0;
-  background: #fff;
-
-  color: #fff;
-  font-family: Arial;
-  font-size: 12px;
-}
-
-.body{
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  right: 0px;
-  bottom: 0px;
-  width: 100%;
-  height: 100%;
-  background-image: url("image/bg.jpg");
-  background-size: cover;
- // -webkit-filter: blur(5px);
-  z-index: 0;
-}
-
-.grad{
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  right: 0px;
-  bottom: 0px;
-  width: auto;
-  height: auto;
-  //background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(100%,rgba(0,0,0,0.65))); /* Chrome,Safari4+ */
-  z-index: 1;
- // opacity: 0.7;
-}
-
-.header{
-  position: absolute;
-  top: calc(50% - 35px);
-  left: calc(50% - 255px);
-  z-index: 2;
-}
-
-.header div{
-  float: left;
-  color: #000;
-  font-family: 'Exo', sans-serif;
-  font-size: 35px;
-  font-weight: 200;
-}
-
-.header div span{
-  color: #5379fa !important;
-}
-
-.login{
-  background-color: white;
-    position: absolute;
-    top: calc(10%);
-    left: calc(40%);
-    height: 270px;
-    width: 270px;
-    padding: 20px;
-    z-index: 2;
-    border-radius: 51px;
-    border-color: black;
-    border: 1px solid black;
-}
-
-.login input[type=text]{
-  width: 250px;
-  height: 30px;
-  background: transparent;
-  border: 1px solid rgba(0,0,0,0.9);
-  border-radius: 2px;
-  color: #000;
-  font-family: 'Exo', sans-serif;
-  font-size: 16px;
-  font-weight: 400;
-  padding: 4px;
-}
-
-.login input[type=password]{
-  width: 250px;
-  height: 30px;
-  background: transparent;
-  border: 1px solid rgba(0,0,0,0.9);
-  border-radius: 2px;
-  color: #000;
-  font-family: 'Exo', sans-serif;
-  font-size: 16px;
-  font-weight: 400;
-  padding: 4px;
-  margin-top: 10px;
-}
-
-.login input[type=button]{
-  width: 126px;
-  height: 40px;
-  background: #b20b2b;
-  border: 1px solid #000;
-  cursor: pointer;
-  border-radius: 2px;
-  color: #000;
-  font-family: 'Exo', sans-serif;
-  font-size: 20px;
-  font-weight: 400;
-  padding: 6px;
-  margin-top: 10px;
-}
-
-.login input[type=button]:hover{
- // opacity: 0.8;
-}
-
-.login input[type=button]:active{
- // opacity: 0.6;
-}
-
-.login input[type=text]:focus{
-  outline: none;
-  border: 1px solid rgba(0,0,0,0.9);
-}
-
-.login input[type=password]:focus{
-  outline: none;
-  border: 1px solid rgba(0,0,0,0.9);
-}
-
-.login input[type=button]:focus{
-  outline: none;
-}
-
-::-webkit-input-placeholder{
-   color: rgba(0,0,0,0.9);
-}
-
-::-moz-input-placeholder{
-   color: rgba(0,0,0,0.9);
-}
-</style>
-
-   
-
-</head>
-
-<body>
-
-  <div class="body"></div>
-    <div class="grad"></div>
-  
-    <br>
-    <form  id='myform' name="myform" method="POST"  class="form-horizontal"  autocomplete="off" >
-    <div class="login">
-        <div><img src="image/cont.png" alt="icon" style="width:250px;height:42px;"></div><br/>
-      <input id="comp_name" type="text"  name="CompanyName" list="CompanyName" placeholder="Company Name" >
-           <datalist id="CompanyName">
-             <?
-			// $con = new mysqli('localhost', 'root', '', 'db_admin');
-             $con = new mysqli('132.148.86.127', 'contracktor', 'Uve52^s6', 'db_admin');
-          $res = $con->query("select * from tbl_company where isactive = 1");
-               while($cam = $res->fetch_object())
-               {
-               ?><option data-xyz="<?=$cam->DBname?>/<?=$cam->Account?>" value="<?=$cam->CompanyName?>" >
-              <?  }  $con->close();   ?>
-            </datalist><br>
-        <input type='hidden' value='' id='cmpnyname' name='cmpnyname'><br>
-        <input type="text" placeholder="username" id="username" name="username"><br>
-        <input type="password" placeholder="password" name="password" id="password" ><br>
-       <input type="button" value="Signup" id="signup"> <input type="button" value="Login" id="login" onclick="validateForm()"> <br>
-        <div id="error" style="<? if($err!='1'){ echo 'display:none;';}?> width:86%; height:60px; top:5px; color:red;"> 
-    <h5 style="width: 107%;font-size:.8rem;">Please enter valid company/username & password<h5>
-      </div> 
-    </div>
-  </form>
-
-  <script src='https://codepen.io/assets/libs/fullpage/jquery.js'></script>
-
-</body>
-
-</html>
+</div><!-- OuterFrmDl END -->
+<div id="FooterBxDL">
+  <p>Copyright © YourCompany </p>
+</div>
+</body></html>
